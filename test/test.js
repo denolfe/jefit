fc098e9f0bfab6c2b55fc7e808442c560a736118
Elliot DeNolf <denolfe@gmail.com>
2015-08-04 23:14:15 -0600

    Clean up some console.logs() and formatted test output

--- test/test.js
+++ test/test.js
@@ -2,13 +2,13 @@ var jefit = require('../index.js');
 var config = require('./config.json');
 
 jefit.fetchSingleDate(config.userId, '2013-06-14', function (result) {
-  console.log(result);
+  console.log(JSON.stringify(result, null, 2));
 });
 
-jefit.fetchSingleDate(config.userId, '2014-01-13', function (result) {
-  console.log(result);
+jefit.fetchSingleDate(config.userId, '2014-01-12', function (result) {
+  console.log(JSON.stringify(result, null, 2));
 });
 
 jefit.fetchSingleDate(config.userId, '2015-08-03', function (result) {
-  console.log(JSON.stringify(result));
+  console.log(JSON.stringify(result, null, 2));
 });
--- test/test2.js
+++ test/test2.js
@@ -2,5 +2,5 @@ var jefit = require('../index.js');
 var config = require('./config.json');
 
 jefit.fetchMostRecent(config.userId, function (result) {
-  console.log(JSON.stringify(result));
+  console.log(JSON.stringify(result, null, 2));
 });
