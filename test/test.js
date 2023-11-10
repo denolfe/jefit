59ee9d4134ea0629b022be1ec3eaa03d4306c239
jjderz <jjd3@njit.edu>
2023-11-10 18:21:28 -0500

    changed fetchSingleDate to callback(error, result) and modified test.js to test it

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 var jefit = require('../index.js');
 var config = require('./config.example.json');
 
-jefit.fetchSingleDate(config.username, '2013-06-14', function (result) {
+jefit.fetchSingleDate(config.username, '2013-06-14', function (error, result) {
   console.log(JSON.stringify(result, null, 2));
 });
 
-jefit.fetchSingleDate(config.username, '2014-01-12', function (result) {
+jefit.fetchSingleDate(config.username, '2014-01-12', function (error, result) {
   console.log(JSON.stringify(result, null, 2));
 });
 
-jefit.fetchSingleDate(config.username, '2015-08-03', function (result) {
+jefit.fetchSingleDate(config.username, '2015-08-03', function (error, result) {
   console.log(JSON.stringify(result, null, 2));
 });
--- test/test2.js
+++ test/test2.js
@@ -1,7 +1,7 @@
 var jefit = require('../index.js');
 var config = require('./config.example.json');
 
-jefit.fetchMostRecent(config.username, function (result) {
+jefit.fetchMostRecent(config.username, function (error, result) {
   console.log(JSON.stringify(result, null, 2));
 
 });
