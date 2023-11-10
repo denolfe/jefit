7131b21051e81fce0f4bf495439679994e53872e
jjderz <jjd3@njit.edu>
2023-11-10 18:18:34 -0500

    Realized the same issue is occurring with fetchSingleDate

--- test/test2.js
+++ test/test2.js
@@ -1,5 +1,5 @@
 var jefit = require('../index.js');
-var config = require('./config.json');
+var config = require('./config.example.json');
 
 jefit.fetchMostRecent(config.username, function (result) {
   console.log(JSON.stringify(result, null, 2));
